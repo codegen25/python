76995c414807d06c0804ce538bac1231a13a276f
codegen25 <72343388+codegen25@users.noreply.github.com>
2020-10-04 15:44:03 +0300

    Add files via upload

---
+++ eleman silme ve ekleme 2.py
@@ -0,0 +1,15 @@
+# liste elemanlarını değiştirme
+
+list=[15,30,45,60]
+print(list)
+
+list[1]=20
+print(list)
+
+#eleman ekleme
+alisveris=["elma","armut", "portakal"]
+print(alisveris)
+alisveris.append("kivi")
+print(alisveris)
+alisveris.insert(1, "kivi")
+print(alisveris)
